a402f6a2b79927fdcbf843f90041c614193c75ab
ListyaDwiSubagya <listyodwi062@gmail.com>
2025-07-06 23:40:04 +0700

    final

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
         <Route path='/movies' element={<Movies/>}/>
         <Route path='/movies/:id' element={<MovieDetails/>}/>
         <Route path='/movies/:id/:date' element={<SeatLayout/>}/>
-        <Route path='/my-bookins' element={<MyBookings/>}/>
+        <Route path='/my-bookings' element={<MyBookings/>}/>
         <Route path='/favorite' element={<Favorite/>}/>
       </Routes>
       {!isAdminRoute && <Footer/>}
